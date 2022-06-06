d439c0190825b0fc76533380ae925f733cd8210d
FebrianAditya <febrian.aditya@student.upi.edu>
2022-06-06 22:20:43 +0700

    initialize code

---
+++ pages/login-page.jsx
@@ -0,0 +1,6 @@
+export default () => (
+    <div>
+        <h1>login-page</h1>
+        <h2>Testing</h2>
+    </div>
+)
---
+++ pages/register-page.js
@@ -0,0 +1,5 @@
+export default () => (
+    <div>
+        <h1>register-page</h1>
+    </div>
+)
